f1e9f3320f78a84210d7ebd69924e9f5bafae70f
scuzzles <68835586+scuzzles@users.noreply.github.com>
2024-01-08 23:34:15 -0600

    updated to work with new website
    updated to contain nahcrofDB file which is designed to work with the nahcrofDB api

--- nahcrofDB.py
+++ nahcrofDB.py
@@ -1,36 +1,43 @@
-import sys
-import os, shutil
-import pickle
 import requests
-utf = "utf-8"
+import json
+import os
+token = [0]
+username = [0]
 
-def makeKey(keyname, keycontent):
-    db = pickle.load(open("main.db", "rb"))
-    db[keyname] = keycontent
-    pickle.dump(db, open("main.db", "wb"))
+# stores token and username to simplify database access
+def init(api_token, location):
+    token[0] = api_token
+    username[0] = location
 
-
+# returns a key from the defined database
 def getKey(keyname):
-    try:
-        content = pickle.load(open("main.db", "rb"))[keyname]
-    except KeyError:
-        content = "Key not found"
-    return content
+    if token[0] == 0:
+        return "token parameter not set"
+    elif username[0] == 0:
+        return "username/location parameter not set"
+    else:
+        r = requests.get(url=f"https://database.nahcrof.com/getKey/['{username[0]}', '{keyname}', '{token[0]}']")
+        data = r.json()
+        return data["keycontent"]
 
-def createDB():
-    db = {}
-    pickle.dump(db, open("main.db", "wb"))
+# creates a key in the defined database
+def makeKey(keyname, keycontent):
+    if token[0] == 0:
+        print("token parameter not set")
+    elif username[0] == 0:
+        print("username/location parameter not set")
+    else:
+        payload = {"location": username[0], "keyname": keyname, "keycontent": keycontent, "token": token[0]}
+        r1 = requests.post('https://database.nahcrof.com/makeKey', json=payload)
+        return r1
 
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    if args:
-        if args[0] == "help":
-            print("Commands:")
-            print("createDB - creates a database to load data from")
-            print("")
-            print("this is currently the only command that exists, more commands will come in the future :)")
-    if args[0] == "createDB":
-        createDB()
-
-
-
+# this will reset the defined database to have 0 keys, no backup will be made, DO NOT use this unless you know what you are doing
+def resetDB():
+    if token[0] == 0:
+        print("token parameter not set")
+    elif username[0] == 0:
+        print("username/location parameter not set")
+    else:
+        payload = {"location": username[0], "token": token[0]}
+        r1 = requests.post('https://database.nahcrof.com/resetDB', json=payload)
+        return r1
