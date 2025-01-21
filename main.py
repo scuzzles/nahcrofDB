77a23354881d070f338d5cc538343c809af2c4db
Tyrae Paul <68835586+scuzzles@users.noreply.github.com>
2025-01-21 10:04:25 -0600

    Update main.py
    fixed api error

--- main.py
+++ main.py
@@ -198,6 +198,7 @@ def home():
             if session["password"] == admin_password:
                 return redirect("/dashboard")
             else:
+                session.clear()
                 return "how?"
 
 @app.route("/dashboard")
@@ -480,7 +481,7 @@ def keysv2(database):
             for x in args:
                 if x in memory_queue:
                     data = memory_queue[x]
-                    if data["location"] == location:
+                    if data["location"] == database:
                         tempdict[x] = data["data"][x]
                     else:
                         templist.append(x)
